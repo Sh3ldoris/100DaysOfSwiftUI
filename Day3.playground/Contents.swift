ca41c6411f9979fa32c93c9d4e4fc670f5cdfbe5
Adam Lany <adam.lany@posam.sk>
2025-06-29 10:59:41 +0200

    day 3 and 4

---
+++ Day3.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+// Define an array of strings
+let myArray: [String] = ["Hello", "World", "Me", "World"]
+
+// Print the number of array's items
+print(myArray.count);
+// Print the number of unique items in array
+print(Set(myArray).count)
