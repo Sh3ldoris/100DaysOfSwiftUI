e06e269e6ba7cd9849368a325f85e21b9338478b
Adam Lany <adam.lany@posam.sk>
2025-06-29 10:04:44 +0200

    Day 1 and 2

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+let celsiusTemperature: Double = 25
+
+let fahrenheitTemperature: Double = celsiusTemperature * 9 / 5 + 32
+
+print("\(celsiusTemperature)°C is equal to \(fahrenheitTemperature)°F")
