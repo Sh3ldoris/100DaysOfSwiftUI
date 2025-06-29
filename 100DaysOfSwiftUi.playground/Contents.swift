74c926fd87340db88f1390d6d0c2d9ef2140c83d
Adam Lany <adam.lany@posam.sk>
2025-06-29 10:03:37 +0200

    Day 1 and 2

--- 100DaysOfSwiftUi.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-import Cocoa
-
-let celsiusTemperature: Double = 25
-
-let fahrenheitTemperature: Double = celsiusTemperature * 9 / 5 + 32
-
-print("\(celsiusTemperature)°C is equal to \(fahrenheitTemperature)°F")
