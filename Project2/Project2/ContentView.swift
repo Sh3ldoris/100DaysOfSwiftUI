96a8e7bd551cf224f9837c4428708c562b427a24
Adam Lany <adam.lany@posam.sk>
2025-07-24 22:36:52 +0200

    Project 2

---
+++ Project2/Project2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by Adam Lany on 24/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project2/Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Adam Lany on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
